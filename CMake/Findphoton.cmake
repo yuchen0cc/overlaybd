b157612562319c2e1baf1208183558dc296252cb
yuchen.cc <yuchen.cc@alibaba-inc.com>
2023-04-06 18:50:39 +0800

    workflow: add build on aarch64 job
    Signed-off-by: yuchen.cc <yuchen.cc@alibaba-inc.com>

--- CMake/Findphoton.cmake
+++ CMake/Findphoton.cmake
@@ -3,8 +3,10 @@ set(FETCHCONTENT_QUIET false)
 
 FetchContent_Declare(
   photon
-  GIT_REPOSITORY https://github.com/alibaba/PhotonLibOS.git
-  GIT_TAG v0.5.6
+  # GIT_REPOSITORY https://github.com/alibaba/PhotonLibOS.git
+  # GIT_TAG v0.5.6
+  GIT_REPOSITORY https://github.com/yuchen0cc/PhotonLibOS.git
+  GIT_TAG 8a1f8f97763286e3a3991b9634b05f62283e9021
 )
 
 if(BUILD_TESTING)
